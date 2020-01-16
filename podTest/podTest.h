bcc906f14f3e060bbc19c013767fb74b3edb9c55
Kasun Sandeep <kasun.emedia@gmail.com>
2020-01-16 16:36:50 +0530

    Initial commit

---
+++ podTest/podTest.h
@@ -0,0 +1,19 @@
+//
+//  podTest.h
+//  podTest
+//
+//  Created by Kasun Sandeep on 1/16/20.
+//  Copyright © 2020 Elegant Media pvt ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for podTest.
+FOUNDATION_EXPORT double podTestVersionNumber;
+
+//! Project version string for podTest.
+FOUNDATION_EXPORT const unsigned char podTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <podTest/PublicHeader.h>
+
+
